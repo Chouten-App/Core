2201977b2ea8e6bf9c401efa5a33dc7557a0b1b3
Nabil <noufnouf703@gmail.com>
2024-12-30 12:32:41 +0100

    Added the mediaData property to CollectionItem

--- Sources/Core/Models/Collection/CollectionItem.swift
+++ Sources/Core/Models/Collection/CollectionItem.swift
@@ -9,11 +9,13 @@ import Foundation
 
 public struct CollectionItem: Codable, Equatable, Sendable {
     public var infoData: InfoData
+    public var mediaData: MediaItem?
     public var url: String
     public var flag: ItemStatus
     
-    public init(infoData: InfoData, url: String, flag: ItemStatus) {
+    public init(infoData: InfoData, url: String, mediaItem: MediaItem? = nil, flag: ItemStatus) {
         self.infoData = infoData
+        self.mediaData = mediaItem
         self.url = url
         self.flag = flag
     }
